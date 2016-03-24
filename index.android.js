f31a34d159fbfdd4cc0e4059468d6a82ea02fff4
Julian Zhou <jzhou2oo6@gmail.com>
2016-03-24 15:49:09 -0400

    Insert Material Design Cards

--- index.android.js
+++ index.android.js
@@ -8,44 +8,41 @@ import React, {
   Component,
   StyleSheet,
   Text,
+  Image,
+  ScrollView,
   View
 } from 'react-native';
-
+import {
+  getTheme
+} from 'react-native-material-kit';
 class WaveLine extends Component {
   render() {
+    var base64Icon = 'http://www.getmdl.io/assets/demos/welcome_card.jpg';
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
+<ScrollView style={styles.scrollView}>
+       <View style={styles.container}>
+         <View style={theme.cardStyle}>
+           <View
+             style={{
+               padding : 15,
+             }}
+             >
+             <Text style={[theme.cardContentStyle, {padding:0}]}>
+             <Text style={{fontWeight: 'bold'}}>
+             Welcome to React Native! {"\n"}
+             </Text>
+             To get started, edit index.android.js {"\n"}
+             Shake or press menu button for dev menu
+             </Text>
+           </View>
+         </View>
+       </View>
+     </ScrollView>
     );
   }
 }
+const theme = getTheme();
+const styles = require('./resources/styles');
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 AppRegistry.registerComponent('WaveLine', () => WaveLine);
